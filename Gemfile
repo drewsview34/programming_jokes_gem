c244711d32e967de70c740202d2d227e3065f287
drewsview34 <drewsview34@gmail.com>
2020-04-10 14:49:33 -0700

    First commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in programming_jokes_gem.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
---
+++ lib/programming_jokes_gem.rb
@@ -0,0 +1,6 @@
+require "programming_jokes_gem/version"
+
+module ProgrammingJokesGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
